60c672b06d8c20ab089ae3c91423d6775c5860d3
leonardo-del-nero <shidejavu4@gmail.com>
2026-05-11 17:55:42 -0300

    minor changes

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -4,7 +4,9 @@ class UserRepository:
         self._db = {
             "1": {"nome": "Neo", "role": "The One", "status": "Awake", "skills": ["Kung Fu", "Flying"]},
             "2": {"nome": "Trinity", "role": "Hacker", "status": "Active", "skills": ["Motorcycles", "Combat"]},
-            "3": {"nome": "Morpheus", "role": "Captain", "status": "Active", "skills": ["Leadership", "Tactics"]}
+            "3": {"nome": "Morpheus", "role": "Captain", "status": "Active", "skills": ["Leadership", "Tactics"]},
+            "4": {"nome": "Piaba", "role": "Fat", "status": "Inactive", "skills": ["Iluminat", "Strongness"]},
+            "5": {"nome": "Pipoca", "role": "Bark", "status": "Active", "skills": ["Iluminat's Daughter", "Loud Bark"]}
         }
 
     def find_by_id(self, user_id):
